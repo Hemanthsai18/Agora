1f30a6c43eceb6fc421d7a04aebbbf050f2f9fa0
hemanth <hemanthchintada@gmail.com>
2021-12-29 14:51:59 +0530

    commit

---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = "dda59bcd840e481db7b9c734704c6a5d";
+const Token = "006dda59bcd840e481db7b9c734704c6a5dIAAEnXYC5Dil2AXeonOtBwmB+lRECCF8oNvUZ5o3qx0si8JBJDUAAAAAEAD4dPjPbdjLYQEAAQBs2Mth";
